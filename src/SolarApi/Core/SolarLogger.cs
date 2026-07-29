165d9844f99d84fbd7fcfac90d825c3732e33249
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-07-28 23:00:09 -0600

    feat: add SolarLogger

---
+++ src/SolarApi/Core/SolarLogger.cs
@@ -0,0 +1,28 @@
+﻿namespace SolarApi;
+
+using global::MelonLoader;
+
+using Microsoft.Extensions.Logging;
+
+using SolarApi.Logging;
+
+internal static class SolarLogger
+{
+    public static ILoggerFactory Factory => field ??= CreateFactory();
+
+    public static ILoggerFactory CreateFactory()
+    {
+        var solarMelon = Melon<Core>.Instance;
+
+        var provider = new MelonLoggerProvider(solarMelon.ConsoleColor);
+
+        var factory = LoggerFactory.Create(
+            builder => builder.AddProvider(provider));
+
+        solarMelon.OnUnregister.Subscribe(
+            factory.Dispose,
+            unsubscribeOnFirstInvocation: true);
+
+        return factory;
+    }
+}
