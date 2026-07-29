770456986086ab521c3f0b4cb2bbdf79178f0570
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-07-28 23:52:21 -0600

    feat: add DisposeGroup

---
+++ src/SolarApi/Core/Collections/DisposeGroup.cs
@@ -0,0 +1,28 @@
+﻿namespace SolarApi.Collections;
+
+public sealed class DisposeGroup(
+    int capacity)
+    : IDisposable
+{
+    private readonly List<IDisposable> disposables = new(capacity);
+
+    public DisposeGroup()
+        : this(0)
+    {
+    }
+
+    public T Collect<T>(T disposable)
+        where T : IDisposable
+    {
+        this.disposables.Add(disposable);
+        return disposable;
+    }
+
+    public void Dispose()
+    {
+        foreach (var disposable in this.disposables)
+        {
+            disposable.Dispose();
+        }
+    }
+}
