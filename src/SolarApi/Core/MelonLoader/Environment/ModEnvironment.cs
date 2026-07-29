9eaa391e5f85057cae0426bb603cacff95b13fa0
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-07-29 00:14:07 -0600

    feat: add ModEnvironment

---
+++ src/SolarApi/Core/MelonLoader/Environment/ModEnvironment.cs
@@ -0,0 +1,34 @@
+﻿namespace SolarApi.MelonLoader.Environment;
+
+using global::MelonLoader.Utils;
+
+public class ModEnvironment
+{
+    public ModEnvironment(string modName)
+    {
+        ArgumentNullException.ThrowIfNull(modName);
+        this.ModName = modName;
+    }
+
+    public string ModName { get; }
+
+    public DirectoryInfo ModUserDataDirectory
+        => field ??= GetDirectoryInternal(MelonEnvironment.UserDataDirectory, this.ModName);
+
+    public DirectoryInfo GetDirectory(bool ensureCreated, params IEnumerable<string> paths)
+    {
+        var directory = GetDirectoryInternal([this.ModUserDataDirectory.FullName, .. paths]);
+        if (ensureCreated)
+        {
+            directory.Create();
+        }
+
+        return directory;
+    }
+
+    private static DirectoryInfo GetDirectoryInternal(params IEnumerable<string> paths)
+    {
+        string path = Path.Join([.. paths]);
+        return new DirectoryInfo(path);
+    }
+}
