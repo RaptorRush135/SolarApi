8a819b5bf19a0a6c9f6d63a58392859ed09dc724
RaptorRush135 <112044769+RaptorRush135@users.noreply.github.com>
2026-08-01 00:36:13 -0600

    feat: add DisposableAction

---
+++ src/SolarApi/Core/Utilities/DisposableAction.cs
@@ -0,0 +1,14 @@
+﻿namespace SolarApi.Utilities;
+
+public sealed class DisposableAction(
+    Action dispose)
+    : IDisposable
+{
+    private Action? dispose = dispose;
+
+    public void Dispose()
+    {
+        this.dispose?.Invoke();
+        this.dispose = null;
+    }
+}
